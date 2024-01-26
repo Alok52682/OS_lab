15f392d7054c4e898f8af1477b6dc8081fa19cb5
Alok Rajbongshi <alokraj00061@.com>
2024-01-26 16:54:41 +0600

    2nd part completed

--- Round_Robin.c
+++ Round_Robin.c
@@ -23,5 +23,43 @@ int main()
         temp[i] = burst_time[i];
     }
 
+    printf("\nEnter Time Quantum: ");
+    scanf("%d", &time_quantum);
+    printf("\nProcess ID\t\tBurst Time\t Turnaround Time\t Waiting Time\n");
+    for (total = 0, i = 0; x != 0;)
+    {
+        if (temp[i] <= time_quantum && temp[i] > 0)
+        {
+            total = total + temp[i];
+            temp[i] = 0;
+            counter = 1;
+        }
+        else if (temp[i] > 0)
+        {
+            temp[i] = temp[i] - time_quantum;
+            total = total + time_quantum;
+        }
+        if (temp[i] == 0 && counter == 1)
+        {
+            x--;
+            printf("\nProcess[%d]\t\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
+            wait_time = wait_time + total - arrival_time[i] - burst_time[i];
+            turnaround_time = turnaround_time + total - arrival_time[i];
+            counter = 0;
+        }
+        if (i == limit - 1)
+        {
+            i = 0;
+        }
+        else if (arrival_time[i + 1] <= total)
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+
     return 0;
 }
